7e4d99d0829320aa1fb79716fb632a9170f2800c
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-08-08 16:05:09 -0400

    feat: insert password

--- src/app/db.py
+++ src/app/db.py
@@ -3,6 +3,10 @@ import os
 
 TABLE_NAME = os.getenv("TABLE")
 
+# set TABLE_NAME to 'TinypassTable' if it is not set
+if TABLE_NAME is None:
+    TABLE_NAME = 'TinyPassDevelop-TableTinyPassDevelopC6549CC7-104JGT0OYRKJR'
+
 def get_dynamodb_client():
     return boto3.client('dynamodb')
 
@@ -17,4 +21,10 @@ def upsert_password(user_id, entry_name, password):
             'Password': {'S': password}
         }
     )
-    print(response)
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        return True
+    else:
+        return False
+    
+if __name__ == "__main__":
+    print(upsert_password('user1', 'entry1', 'password1'))
--- src/app/main.py
+++ src/app/main.py
@@ -1,27 +1,35 @@
 import boto3
 import db
+import json
 
 
 def handler(event, context):
     api_path = event.get('path', '')
 
     # Get JSON body of HTTP request
-    body = event.get('body', '{}')
+    body = json.loads(event.get('body', '{}'))
 
     # Get user_id, entry_name, and password from JSON body
-    user_id = body.get('user_id', '')
-    entry_name = body.get('entry_name', '')
-    password = body.get('password', '')
+    UserID = body.get('UserID', '')
+    EntryName = body.get('EntryName', '')
+    Password = body.get('Password', '')
 
     # What request is it (GET, POST, PUT, DELETE)?
     method = event.get('httpMethod', '')
 
     match method:
-        case 'GET':
+        case 'POST':
             # Insert password for user1, entry1, password1 through JSON body of HTTP request
-            
-
-            db.upsert_password('user1', 'entry1', 'password1')
+            result = db.upsert_password(UserID, EntryName, Password)
+            if result:
+                return {
+                    'statusCode': 200,
+                    'body': 'Password inserted\n'
+                }
+            return {
+                'statusCode': 400,
+                'body': 'Error: The Password was not successfully inserted\n'
+            }
 
     # match api_path:
     #     case '/':
